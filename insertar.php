e83179278ff8c1f21c7c7bdfb651e2ecab6fcf2c
especializacionmovil <especializacionmovil@gmail.com>
2016-11-29 16:07:58 -0500

    inserta  mascota

--- insertar.php
+++ insertar.php
@@ -1,6 +1,15 @@
 <?php
 include ('Conectarse.php');
 $conexion = Conectar();
-$insertar = "";
+$nombre = $_POST["nombre"];
+$tipo = $_POST["tipo"];
+$raza = $_POST["raza"];
+$fecha_nac = $_POST["fecha_nac"];
+$descripcion = $_POST["descripcion"];
+$urlimagen = $_POST["urlimagen"];
+
+print_r($_POST);
+
+$insertar = "INSERT INTO mascota (nombre, tipo, raza, fecha_nac, descripcion, urlimagen) VALUES('$nombre','$tipo','$raza','$fecha_nac','$descripcion','$urlimagen')";
 mysqli_query($conexion, $insertar);
 mysqli_close($conexion);
